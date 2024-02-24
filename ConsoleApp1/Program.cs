d7220fdb46507a0e43fe383b4e5113f6638a53e6
79612 <79612@DESKTOP-O1P5TC5>
2024-02-24 15:00:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// for т.к. можно задать параметры работы цикла
+
+            int minValue = 5, maxValue = 103, numberStep = 7;
+
+            for (int i = minValue; i <= maxValue; i += numberStep)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
